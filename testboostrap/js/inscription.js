3b84bcd0350d71e50a06fc5c4e8e6910c62d658d
leo <thomasleo723@gmail.com>
2026-03-03 19:10:12 +0100

    code digisiad login-entreprises-inscript

---
+++ testboostrap/js/inscription.js
@@ -0,0 +1,53 @@
+const boutton1 = document.getElementById('premierbouton');
+const boutton2 = document.getElementById('secondbouton');
+
+const partie1 = document.getElementById('première');
+const partie2 = document.getElementById('deuxieme');
+const partie3 = document.getElementById('third');
+
+
+partie2.hidden = true;
+partie3.hidden = true;
+
+
+boutton1.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const inputs = partie1.querySelectorAll('input');
+    let valide = true;
+
+    for (let input of inputs) {
+        if (input.value.trim() === '') {
+            valide = false;
+            input.classList.add('border-danger');
+        } else {
+            input.classList.remove('border-danger');
+        }
+    }
+
+    if (valide) {
+        partie1.hidden = true;
+        partie2.hidden = false;
+    }
+});
+
+boutton2.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const inputs = partie2.querySelectorAll('input');
+    let valide = true;
+
+    for (let input of inputs) {
+        if (input.value.trim() === '') {
+            valide = false;
+            input.classList.add('border-danger');
+        } else {
+            input.classList.remove('border-danger');
+        }
+    }
+
+    if (valide) {
+        partie2.hidden = true;
+        partie3.hidden = false;
+    }
+});
